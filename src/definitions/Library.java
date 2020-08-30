08070530f5d0c5b7759fb8f13d84604152957bb5
pragya <pragya.sharma2_cs19@gla.ac.in>
2020-08-30 07:41:20 +0530

    change the way of writing the names of constants.

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -12,16 +12,16 @@ import java.util.Objects;
 public class Library {
     private Book[] availableBooks;
     private int numberOfAvailableBooks;
-    private final String[] availableBooksList = {"Let us Python", "Let us C", "Pointer in C", "Database Management System", "SOftware Engineering"};
-    private final String[] authorsCorrespondingToAvailableBooks = {"Yashwant kanetkar", "Yashwant kanetkar", "Yashwant kanetkar", "f.Korth and Navathe", "Somervielle"};
-    private final String[] isbnNumberCorrespondingToAvailableBooks = {"9897400406763", "9557620104453", "6788586790345", "8195175385245", "9518524567535"};
+    private final String[] AVAILABLE_BOOKS = {"Let us Python", "Let us C", "Pointer in C", "Database Management System", "SOftware Engineering"};
+    private final String[] AUTHOR_OF_THE_BOOKS = {"Yashwant kanetkar", "Yashwant kanetkar", "Yashwant kanetkar", "f.Korth and Navathe", "Somervielle"};
+    private final String[] ISBN_NUMBER_OF_BOOKS = {"9897400406763", "9557620104453", "6788586790345", "8195175385245", "9518524567535"};
 
     //Constructor
     public Library() {
         numberOfAvailableBooks = 5;
         availableBooks = new Book[numberOfAvailableBooks];
         for (int index = 0; index < availableBooks.length; index++) {
-            availableBooks[index] = new Book(availableBooksList[index], authorsCorrespondingToAvailableBooks[index], isbnNumberCorrespondingToAvailableBooks[index]);
+            availableBooks[index] = new Book(AVAILABLE_BOOKS[index], AUTHOR_OF_THE_BOOKS[index], ISBN_NUMBER_OF_BOOKS[index]);
         }
     }
 
