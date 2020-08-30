37bccb89c70c0a1cd95fbb71e32ef46d606810de
pragya <pragya.sharma2_cs19@gla.ac.in>
2020-08-30 07:22:24 +0530

    create constructor.

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -12,4 +12,13 @@ public class Library {
     private final String[] availableBooksList = {"Let us Python", "Let us C", "Pointer in C", "Database Management System", "SOftware Engineering"};
     private final String[] authorsCorrespondingToAvailableBooks = {"Yashwant kanetkar", "Yashwant kanetkar", "Yashwant kanetkar", "f.Korth and Navathe", "Somervielle"};
     private final String[] isbnNumberCorrespondingToAvailableBooks = {"9897400406763", "9557620104453", "6788586790345", "8195175385245", "9518524567535"};
+
+    //Constructor
+    public Library() {
+        numberOfAvailableBooks = 5;
+        availableBooks = new Book[numberOfAvailableBooks];
+        for (int index = 0; index < availableBooks.length; index++) {
+            availableBooks[index] = new Book(availableBooksList[index], authorsCorrespondingToAvailableBooks[index], isbnNumberCorrespondingToAvailableBooks[index]);
+        }
+    }
 }
