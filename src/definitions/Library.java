afb4d0cb493996c2f4681befe8e751e4051d8046
pragya <pragya.sharma2_cs19@gla.ac.in>
2020-08-30 07:21:18 +0530

    create private fields foe arrays to store names ,author,isbn number of the books.

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -8,4 +8,8 @@ package definitions;
 
 public class Library {
     private Book[] availableBooks;
+    private int numberOfAvailableBooks;
+    private final String[] availableBooksList = {"Let us Python", "Let us C", "Pointer in C", "Database Management System", "SOftware Engineering"};
+    private final String[] authorsCorrespondingToAvailableBooks = {"Yashwant kanetkar", "Yashwant kanetkar", "Yashwant kanetkar", "f.Korth and Navathe", "Somervielle"};
+    private final String[] isbnNumberCorrespondingToAvailableBooks = {"9897400406763", "9557620104453", "6788586790345", "8195175385245", "9518524567535"};
 }
