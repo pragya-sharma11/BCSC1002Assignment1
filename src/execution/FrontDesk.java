88446692df93a98f9bf3606015f04eb14a796be6
pragya <pragya.sharma2_cs19@gla.ac.in>
2020-08-30 07:44:53 +0530

    create constants to hold the choices.

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -12,9 +12,16 @@ import definitions.Student;
 import java.util.Scanner;
 
 public class FrontDesk {
+    private static final int NEW_BOOK = 1;
+    private static final int PREVIOUS_BOOK_RETURN = 2;
+    private static final int ALL_BOOKS_LIST = 3;
+    private static final int EXIT = 4;
+
     public static void main(String[] args) {
         Student student = new Student();
         Library library = new Library();
         Scanner scanner = new Scanner(System.in);
+        int choiceNumber;
+        System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
     }
 }
